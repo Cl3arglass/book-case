2f82b27ecf7501d098d499fb9569c6638e6f8cfa
Holden Altaffer <haltaffer@Holdens-MacBook-Pro.local>
2018-07-12 15:17:44 -0700

    add login routes and view form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,4 +19,21 @@ class UsersController < ApplicationController
    end
  end
 
+ get '/login' do
+   if !logged_in?
+     erb :'users/login'
+   else
+     redirect to '/crates'
+   end
+ end
+
+ post '/login' do
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to "/crates"
+    else
+      redirect to '/signup'
+    end
+  end
 end
