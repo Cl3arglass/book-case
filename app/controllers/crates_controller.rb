9cf5c717b17a2ec38717e6767f033e85e1350aa3
Holden Altaffer <haltaffer@Holdens-MacBook-Pro.local>
2018-07-17 19:22:00 -0700

    degug edit, crate creation, and add edit patch

--- app/controllers/crates_controller.rb
+++ app/controllers/crates_controller.rb
@@ -38,12 +38,41 @@ class CratesController < ApplicationController
     end
   end
 
+  patch '/crates/:id' do
+   if logged_in?
+     if params[:name] == ""
+       redirect to "/crates/#{params[:id]}/edit"
+     else
+       @crate = Crate.find_by_id(params[:id])
+       if @crate && @crate.user_id == current_user.id
+         if @crate.update(name: params[:name])
+           redirect to "/crates/#{@crate.id}"
+         else
+           redirect to "/crates/#{@crate.id}/edit"
+         end
+       else
+         redirect to '/crates'
+       end
+     end
+   else
+     redirect to '/login'
+   end
+ end
+
   post "/crates" do
-    if !logged_in?
-     redirect "/login"
+    if logged_in?
+      if params[:name] == ""
+        redirect to "/crates/new"
+      else
+        @crate = current_user.crates.build(name: params[:name])
+        if @crate.save
+          redirect to "/crates/#{@crate.id}"
+        else
+          redirect to "/crates/new"
+        end
+      end
     else
-     Crate.create(params)
-     redirect "/crates"
+      redirect to '/login'
     end
   end
 
