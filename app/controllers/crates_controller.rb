8419d9e07a420b26a25cbacc6422c8125238bd65
Holden Altaffer <haltaffer@Holdens-MacBook-Pro.local>
2018-07-17 18:53:30 -0700

    add edit form and get editn route for a single crate

--- app/controllers/crates_controller.rb
+++ app/controllers/crates_controller.rb
@@ -26,6 +26,18 @@ class CratesController < ApplicationController
     end
   end
 
+  get "/crates/:id/edit" do
+     @crate = Crate.find_by(id: params[:id])
+    if !logged_in?
+     redirect "/login"
+   elsif  @crate && @crate.user_id == current_user.id
+     erb :'crates/edit'
+   else
+     flash[:message] = "Crate not found!"
+     redirect "/crates"
+    end
+  end
+
   post "/crates" do
     if !logged_in?
      redirect "/login"
