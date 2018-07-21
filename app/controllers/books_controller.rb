6aee2ffa0bbc79df5f2efa53db2fc2fc54682a08
Cl3arglass <haltaffer@gmail.com>
2018-07-21 14:35:21 -0700

    add books get route and index page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,10 @@
 class BooksController < ApplicationController
+  get "/books" do
+   if !logged_in?
+    redirect "/login"
+   else
+     @books = Book.all
+     erb :'books/index'
+   end
+ end
 end
