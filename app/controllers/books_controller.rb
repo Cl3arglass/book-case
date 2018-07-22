d03f6452e4fb1b5dd2a8e82a25bbd2b50d9626b7
Cl3arglass <haltaffer@gmail.com>
2018-07-21 17:33:58 -0700

    add book new view and get post routes

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,4 +7,31 @@ class BooksController < ApplicationController
      erb :'books/index'
    end
  end
+
+ get "/books/new" do
+   if !logged_in?
+    redirect "/login"
+   else
+    erb :'books/new'
+   end
+  end
+
+  post "/books" do
+    binding.pry
+    if logged_in?
+      if params[:name] == "" || params[:name] == "" || params[:crate_id] == nil
+        flash[:message] = "Invalid book"
+        redirect to "/books/new"
+      else
+        @book = Book.create(params)
+        if @book.save
+          redirect to "/books/#{@book.id}"
+        else
+          redirect to "/books/new"
+        end
+      end
+    else
+      redirect to '/login'
+    end
+  end
 end
